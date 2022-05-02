75293eb85275e383bf7215e9647eced6656578f9
zhengxianghan97 <zhengxianghan97@foxmail.com>
2022-05-02 08:13:02 +0800

    Summary:modify debug.h
    Project:
    BranchName: master
    WorkPackageName:
    BUGID:

--- include/debug.h
+++ include/debug.h
@@ -13,26 +13,28 @@
 //#define DEBUG_ERROR (1 << 0)
 //#define DEBUG_WARN (1 << 1)
 //#define DEBUG_INFO (1 << 2)
-
+#ifndef DEBUG
+#define DEBUG (0)
+#endif // !DEBUG
 // __UINT8_TYPE__ g_debug_flag = 0;
 
 #define DEBUG_WARNING(format, ...)                                                                 \
     do {                                                                                           \
-        if (1) {                                                                                   \
+        if (DEBUG) {                                                                               \
             printf("[WARN] [%s:%d] " format "\n", __FILE__, __LINE__, ##__VA_ARGS__);              \
         }                                                                                          \
     } while (0)
 
 #define DEBUG_ERROR(format, ...)                                                                   \
     do {                                                                                           \
-        if (1) {                                                                                   \
+        if (DEBUG) {                                                                               \
             printf("[ERROR] [%s:%d] " format "\n", __FILE__, __LINE__, ##__VA_ARGS__);             \
         }                                                                                          \
     } while (0)
 
 #define DEBUG_INFO(format, ...)                                                                    \
     do {                                                                                           \
-        if (1) {                                                                                   \
+        if (DEBUG) {                                                                               \
             printf("[INFO] [%s:%d] " format "\n", __FILE__, __LINE__, ##__VA_ARGS__);              \
         }                                                                                          \
     } while (0)
